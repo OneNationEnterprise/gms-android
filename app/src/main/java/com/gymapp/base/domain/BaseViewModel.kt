8fe6f138480a8d68b5f8a899ea0c0c66b9a866e2
xghita <contact.istrate@gmail.com>
2020-07-07 17:31:51 +0300

    - update schema

--- app/src/main/java/com/gymapp/base/domain/BaseViewModel.kt
+++ app/src/main/java/com/gymapp/base/domain/BaseViewModel.kt
@@ -1,10 +1,5 @@
 package com.gymapp.base.domain
 
-import android.app.Application
-import android.content.Context
-import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.ViewModel
-import com.gymapp.main.GymApplication
-import com.gymapp.main.network.ApiManagerInterface
 
 open class BaseViewModel() : ViewModel()
